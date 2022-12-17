caa6f0988455bc7d315bfff3e248653c0751f0aa
Iryna Artymuk <artimyk@ukr.net>
2022-12-17 08:59:29 +0300

    add local storage

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,26 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-import SimpleLightbox from "simplelightbox";
-
-console.log( SimpleLightbox);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(SimpleLightbox);
 
 const gallery = document.querySelector('.gallery');
 
 function creatMarkup(itemsArr) {
-  const markUp = itemsArr.map(
-    ({ preview, original, description }) =>
-      `<a href="${original}"><img src="${preview}" alt="${description}"" /></a>`
-  ).join("")
-    console.log(markUp)
-    gallery.insertAdjacentHTML('beforeend',markUp)  
+  const markUp = itemsArr
+    .map(
+      ({ preview, original, description }) =>
+        `<a href="${original}"><img src="${preview}" alt="${description}"" /></a>`
+    )
+    .join('');
+  console.log(markUp);
+  gallery.insertAdjacentHTML('beforeend', markUp);
 }
 
 creatMarkup(galleryItems);
 
-
 let modalItem = new SimpleLightbox('.gallery a', {
-    overlayOpacity: 0.8,
-    captionsData: 'alt',
-
-  });
+  overlayOpacity: 0.8,
+  captionsData: 'alt',
+  captionDelay: 0,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('#vimeo-player');
+const options = {
+  autoplay: true,
+  t: 5,
+  transparent: false,
+};
 
+const player = new Player(iframe, options);
+
+const getTime = function (event) {
+  const playTime = event.seconds;
+    console.log(playTime)
+    localStorage.setItem("videoplayer-current-time", event.seconds)
+
+};
+
+function updateTime(event) {
+    event.seconds = localStorage.getItem("videoplayer-current-time") || "";
+  }
+
+player.on('timeupdate', getTime);
+player.on('play', updateTime);
+
+
+
+
+// setCurrentTime()
