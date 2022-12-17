499ac0d332985c98f3c96e63107afe027285e1c3
Iryna Artymuk <artimyk@ukr.net>
2022-12-17 07:15:50 +0300

    simpl.box

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,26 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-console.log(`hello world`)
-console.log(`hello world2`)
+import SimpleLightbox from "simplelightbox";
+
+console.log( SimpleLightbox);
+
+const gallery = document.querySelector('.gallery');
+
+function creatMarkup(itemsArr) {
+  const markUp = itemsArr.map(
+    ({ preview, original, description }) =>
+      `<a href="${original}"><img src="${preview}" alt="${description}"" /></a>`
+  ).join("")
+    console.log(markUp)
+    gallery.insertAdjacentHTML('beforeend',markUp)  
+}
+
+creatMarkup(galleryItems);
+
+
+let modalItem = new SimpleLightbox('.gallery a', {
+    overlayOpacity: 0.8,
+    captionsData: 'alt',
+
+  });
