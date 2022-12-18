9a6008d467f4b4d3e5bb77b2f885fc38d9603729
Iryna Artymuk <artimyk@ukr.net>
2022-12-18 21:27:09 +0300

    update files

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,14 @@
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('.email');
 const message = document.querySelector('.message');
+const button = document.querySelector('button');
 
 updateInput();
 
-form.addEventListener('input', getDataOnInput);
+form.addEventListener('input', setDataOnInput);
 form.addEventListener('submit', resetOnSubmit);
 
-
-
-
-
-function getDataOnInput(event) {
+function setDataOnInput(event) {
   const {
     elements: { email, message },
   } = event.currentTarget;
@@ -22,23 +19,24 @@ function getDataOnInput(event) {
   };
 
   localStorage.setItem('feedback-form-state', JSON.stringify(userData));
- 
-
 }
 
 function updateInput() {
   const UserDataJSON = localStorage.getItem('feedback-form-state');
   const UserDataParsed = JSON.parse(UserDataJSON);
 
-//   console.log(UserDataParsed.userEmail)
-//   console.log( UserDataParsed.userMessage)
-
-  form.elements.email.value=UserDataParsed.userEmail || ""
-  form.elements.message.value=UserDataParsed.userMessage ||""
-
+  form.elements.email.value = UserDataParsed.userEmail || '';
+  form.elements.message.value = UserDataParsed.userMessage || '';
 }
+
 function resetOnSubmit() {
-    console.log (form.elements.email.value)
-    form.elements.email.value= ""
-    form.elements.message.value= ""
+  Event.preventDefault();
+  const UserDataJSON = localStorage.getItem('feedback-form-state');
+  const UserDataParsed = JSON.parse(UserDataJSON);
+
+  console.log(UserDataParsed.userEmail);
+  console.log(UserDataParsed.userMessage);
+
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
 }
