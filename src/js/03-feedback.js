0a3ffaeaca69b57ccac2661105a6e85e7b2e6d17
Iryna Artymuk <artimyk@ukr.net>
2022-12-18 16:01:20 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.email');
+const message = document.querySelector('.message');
 
+updateInput();
+
+form.addEventListener('input', getDataOnInput);
+form.addEventListener('submit', resetOnSubmit);
+
+
+
+
+
+function getDataOnInput(event) {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  const userData = {
+    userEmail: email.value,
+    userMessage: message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+ 
+
+}
+
+function updateInput() {
+  const UserDataJSON = localStorage.getItem('feedback-form-state');
+  const UserDataParsed = JSON.parse(UserDataJSON);
+
+//   console.log(UserDataParsed.userEmail)
+//   console.log( UserDataParsed.userMessage)
+
+  form.elements.email.value=UserDataParsed.userEmail || ""
+  form.elements.message.value=UserDataParsed.userMessage ||""
+
+}
+function resetOnSubmit() {
+    console.log (form.elements.email.value)
+    form.elements.email.value= ""
+    form.elements.message.value= ""
+}
